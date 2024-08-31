e284f9e91158be576439ade95cadf84360e9c6a1
Artem Bagin <execoq@gmail.com>
2024-08-31 17:25:25 +0500

    added 'Donate' button on home_view

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' as m;
-import 'package:Spheroscopic/modules/snackbar.dart';
+import 'package:Spheroscopic/utils/snackBar.dart';
 import 'package:Spheroscopic/panorama/select_panorama.dart';
 import 'package:Spheroscopic/riverpod/photoState.dart';
 import 'package:Spheroscopic/utils/consts.dart';
@@ -8,6 +9,8 @@ import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:Spheroscopic/riverpod/brightness.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -28,6 +31,104 @@ class _HomeScreen extends ConsumerState<HomeScreen> {
     args = widget.args!;
 
     super.initState();
+
+    startTime();
+  }
+
+  void startTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool firstTime = prefs.getBool('first_time') ?? false;
+
+    if (!firstTime && mounted) {
+      bool isDarkMode = ref.watch(brightnessRef) == Brightness.dark;
+      await showDialog<String>(
+        context: context,
+        dismissWithEsc: false,
+        builder: (context) => ContentDialog(
+          title: const Text('Hey!'),
+          content: RichText(
+            text: TextSpan(
+              text:
+                  'Before you start using Spheroscopic, you need to read the ',
+              style: TextStyle(
+                color: TColor.secondColorText(isDarkMode),
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+              ),
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'Terms of Use',
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      launchUrl(Uri.parse(
+                          'https://spheroscopic.github.io/terms-of-use'));
+                    },
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                    color: Colors.blue,
+                  ),
+                ),
+                TextSpan(
+                  text: ' and ',
+                  style: TextStyle(
+                    color: TColor.secondColorText(isDarkMode),
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                  ),
+                ),
+                TextSpan(
+                  text: 'Privacy Policy',
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      launchUrl(Uri.parse(
+                          'https://spheroscopic.github.io/privacy-policy'));
+                    },
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                    color: Colors.blue,
+                  ),
+                ),
+                TextSpan(
+                  text:
+                      '. If you do not agree to them, please refrain from using our application.',
+                  style: TextStyle(
+                    color: TColor.secondColorText(isDarkMode),
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            FilledButton(
+              style: const ButtonStyle(
+                textStyle: WidgetStatePropertyAll(
+                  TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                padding: WidgetStatePropertyAll(
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                ),
+              ),
+              child: const Text('Agree'),
+              onPressed: () async {
+                await prefs.setBool('first_time', true);
+                Sentry.metrics().increment(
+                  'first.time', // key
+                  value: 1, // value
+                );
+                Navigator.of(context, rootNavigator: true).pop();
+              },
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   @override
@@ -89,35 +190,68 @@ class SelectContainer extends ConsumerWidget {
           bottom: 0,
           child: Padding(
             padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Text(
-                  appVersion,
-                  style: TextStyle(
-                    color: TColor.secondColorText(isDarkMode),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                IconButton(
-                  icon: Image.asset(
-                    "assets/img/github-logo.png",
-                    width: 24,
-                    height: 24,
-                    color: TColor.secondColorText(isDarkMode),
-                  ),
-                  onPressed: () async {
-                    Uri url = Uri.parse(
-                        'https://github.com/Spheroscopic/Spheroscopic');
+            child: SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  FilledButton(
+                    style: const ButtonStyle(
+                      textStyle: WidgetStatePropertyAll(
+                        TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      padding: WidgetStatePropertyAll(
+                        EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+                      ),
+                    ),
+                    onPressed: () async {
+                      Uri url = Uri.parse('https://ko-fi.com/consequential');
 
-                    if (!await launchUrl(url)) {
-                      openSnackBar(
-                          title: 'Error:',
-                          text: 'Could not open url: $url',
-                          context: context);
-                    }
-                  },
-                ),
-              ],
+                      if (!await launchUrl(url)) {
+                        openSnackBar('Could not open url', '$url', context);
+                      }
+                    },
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/img/kofi-logo.png",
+                          width: 24,
+                          height: 24,
+                        ),
+                        const SizedBox(width: 6),
+                        const Text('Donate'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  IconButton(
+                    icon: Image.asset(
+                      "assets/img/github-logo.png",
+                      width: 24,
+                      height: 24,
+                      color: TColor.secondColorText(isDarkMode),
+                    ),
+                    onPressed: () async {
+                      Uri url = Uri.parse(
+                          'https://github.com/Spheroscopic/Spheroscopic');
+
+                      if (!await launchUrl(url)) {
+                        openSnackBar('Could not open url', '$url', context);
+                      }
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    appVersion,
+                    style: TextStyle(
+                      color: TColor.secondColorText(isDarkMode),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                ],
+              ),
             ),
           ),
         ),
@@ -130,7 +264,7 @@ class SelectContainer extends ConsumerWidget {
                 size: 100,
                 color: TColor.mainColor(isDarkMode),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 15),
               Text(
                 "Select or drop panoramas here",
                 style: TextStyle(
@@ -140,16 +274,16 @@ class SelectContainer extends ConsumerWidget {
                 ),
               ),
               const SizedBox(height: 25),
-              FilledButton(
+              Button(
                 style: const ButtonStyle(
                   textStyle: WidgetStatePropertyAll(
                     TextStyle(
                       fontSize: 14,
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                   padding: WidgetStatePropertyAll(
-                    EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    EdgeInsets.symmetric(vertical: 8, horizontal: 15),
                   ),
                 ),
                 onPressed: addPhotoButtonState.isLoading
